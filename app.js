f7e8c9e2e4c79609e498ed126633b25cb9a20568
Lee Vartha <lee-vartha@live.nmit.ac.nz>
2024-03-01 14:37:22 +1300

    adding more stuff to app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+
 const excelRoutes = require('./routes/excelRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/upload', uploadRoutes);
 app.use('/api/excel', excelRoutes) 
 
 app.listen(port, () => {
