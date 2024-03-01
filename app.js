1418c1a97b54fb2f000e67fc6aeab842b3a1af3d
Lee Vartha <lee-vartha@live.nmit.ac.nz>
2024-03-01 13:21:40 +1300

    adding detail to app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const excelRoutes = require('./routes/excelRoutes');
 const app = express();
+const port = 3000;
+
+app.use('/api/excel', excelRoutes) 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
